aa668a7cc173e42b1e9c9bd81c28558805eac8cd
Richard Mulero <mulerolp@gmail.com>
2024-06-15 20:54:43 -0300

    fix: removendo builder das DTOs

--- src/main/java/br/com/mulero/miniautorizador/dto/CardDTO.java
+++ src/main/java/br/com/mulero/miniautorizador/dto/CardDTO.java
@@ -6,12 +6,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CardDTO {
--- src/main/java/br/com/mulero/miniautorizador/dto/TransactionDTO.java
+++ src/main/java/br/com/mulero/miniautorizador/dto/TransactionDTO.java
@@ -3,12 +3,14 @@ package br.com.mulero.miniautorizador.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
