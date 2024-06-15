3ba1d976be35255801c75c6cabe45977051ca58b
Richard Mulero <mulerolp@gmail.com>
2024-06-15 20:06:38 -0300

    chore: atualizando class-name de controller/service/repository

--- src/main/java/br/com/mulero/miniautorizador/domain/repository/CardRepository.java
+++ src/main/java/br/com/mulero/miniautorizador/domain/repository/CardRepository.java
@@ -1,9 +1,9 @@
 package br.com.mulero.miniautorizador.domain.repository;
 
-import br.com.mulero.miniautorizador.domain.entity.Cartao;
+import br.com.mulero.miniautorizador.domain.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartaoRepository extends JpaRepository<Cartao, Long> {
+public interface CardRepository extends JpaRepository<Card, Long> {
 }
