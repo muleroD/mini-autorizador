ffab23d06d7fca9c7735e07f5504b34f3fc1f796
Richard Mulero <mulerolp@gmail.com>
2024-06-15 20:07:23 -0300

    feat: criando service inicial para autorização de transações

---
+++ src/main/java/br/com/mulero/miniautorizador/service/TransactionService.java
@@ -0,0 +1,14 @@
+package br.com.mulero.miniautorizador.service;
+
+import br.com.mulero.miniautorizador.dto.TransactionDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+    public void authorize(TransactionDTO transactionDTO) {
+
+    }
+}
