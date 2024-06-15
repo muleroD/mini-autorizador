116f18dfd046bc92203dc8b2cf8f2f6b50da5ca4
Richard Mulero <mulerolp@gmail.com>
2024-06-15 19:52:55 -0300

    feat: criando exceptions para as regras de autorização

---
+++ src/main/java/br/com/mulero/miniautorizador/infrastructure/exception/SenhaIncorretaException.java
@@ -0,0 +1,7 @@
+package br.com.mulero.miniautorizador.infrastructure.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class SenhaIncorretaException extends TransacaoException {
+}
---
+++ src/main/java/br/com/mulero/miniautorizador/infrastructure/exception/TransacaoException.java
@@ -0,0 +1,7 @@
+package br.com.mulero.miniautorizador.infrastructure.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class TransacaoException extends RuntimeException {
+}
