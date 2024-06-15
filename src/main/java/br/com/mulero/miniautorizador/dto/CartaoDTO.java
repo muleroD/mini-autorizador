4771253ac7f87cdca6a321a7ed34eb4908253baa
Richard Mulero <mulerolp@gmail.com>
2024-06-15 02:46:23 -0300

    feat: incluindo validação de CartaoDTO

--- src/main/java/br/com/mulero/miniautorizador/dto/CartaoDTO.java
+++ src/main/java/br/com/mulero/miniautorizador/dto/CartaoDTO.java
@@ -1,18 +1,18 @@
 package br.com.mulero.miniautorizador.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record CartaoDTO(
-        @NotNull
-        @NotEmpty
-        @Size(min = 16, max = 16)
+        @NotBlank(message = "O número do cartão é obrigatório")
+        @Size(min = 16, max = 16, message = "O número do cartão deve ter 16 dígitos")
+        @Schema(description = "Número do cartão", example = "1234567890123456")
         String numeroCartao,
 
-        @NotNull
-        @NotEmpty
-        @Size(min = 4, max = 4)
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 4, max = 4, message = "A senha deve ter 4 dígitos")
+        @Schema(description = "Senha do cartão", example = "1234")
         String senha
 ) {
 }
