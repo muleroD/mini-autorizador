8dd9173d4c13cadbaae5009aeec87c9a1dd4b652
Richard Mulero <mulerolp@gmail.com>
2024-06-15 02:47:39 -0300

    chore: definindo schema dos endpoints CartaoController

--- src/main/java/br/com/mulero/miniautorizador/controller/CartaoController.java
+++ src/main/java/br/com/mulero/miniautorizador/controller/CartaoController.java
@@ -3,9 +3,12 @@ package br.com.mulero.miniautorizador.controller;
 import br.com.mulero.miniautorizador.dto.CartaoDTO;
 import br.com.mulero.miniautorizador.service.CartaoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,20 +26,26 @@ public class CartaoController {
     @PostMapping
     @Operation(summary = "operation.card.create.summary")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "operation.card.create.success"),
-            @ApiResponse(responseCode = "401", description = "operation.card.create.unauthorized"),
-            @ApiResponse(responseCode = "422", description = "operation.card.create.unprocessableEntity")
+            @ApiResponse(responseCode = "201", description = "operation.card.create.success",
+                    content = @Content(schema = @Schema(implementation = CartaoDTO.class))),
+            @ApiResponse(responseCode = "422", description = "operation.card.create.unprocessableEntity",
+                    content = @Content(schema = @Schema(implementation = CartaoDTO.class))),
+            @ApiResponse(responseCode = "401", description = "operation.card.create.unauthorized",
+                    content = @Content(schema = @Schema()))
     })
-    public ResponseEntity<CartaoDTO> criarCartao(@RequestBody CartaoDTO cartaoDTO) {
+    public ResponseEntity<CartaoDTO> criarCartao(@Valid @RequestBody CartaoDTO cartaoDTO) {
         return cartaoService.salvar(cartaoDTO);
     }
 
     @GetMapping("/{numeroCartao}")
     @Operation(summary = "operation.card.balance.summary")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "operation.card.balance.success"),
-            @ApiResponse(responseCode = "401", description = "operation.card.balance.unauthorized"),
-            @ApiResponse(responseCode = "404", description = "operation.card.balance.notFound")
+            @ApiResponse(responseCode = "200", description = "operation.card.balance.success",
+                    content = @Content(schema = @Schema(implementation = BigDecimal.class))),
+            @ApiResponse(responseCode = "404", description = "operation.card.balance.notFound",
+                    content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "401", description = "operation.card.balance.unauthorized",
+                    content = @Content(schema = @Schema()))
     })
     public ResponseEntity<BigDecimal> obterSaldo(@PathVariable String numeroCartao) {
         return cartaoService.obterSaldo(numeroCartao);
