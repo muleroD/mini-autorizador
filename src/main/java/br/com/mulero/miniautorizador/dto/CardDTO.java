074256e51b24a55a83e606b6a7a7294698316d82
Richard Mulero <mulerolp@gmail.com>
2024-06-16 03:21:59 -0300

    fix: utilizando @Pattern apenas para validação de min/max e numerico

--- src/main/java/br/com/mulero/miniautorizador/dto/CardDTO.java
+++ src/main/java/br/com/mulero/miniautorizador/dto/CardDTO.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,14 +14,12 @@ import lombok.NoArgsConstructor;
 public class CardDTO {
 
     @NotBlank(message = "validation.card.number.notBlank")
-    @Size(min = 16, max = 16, message = "validation.card.number.size")
     @Pattern(regexp = "\\d{16}", message = "validation.card.number.pattern")
     @Schema(description = "Número do cartão", example = "1234567890123456")
     @JsonProperty("numeroCartao")
     String cardNumber;
 
     @NotBlank(message = "validation.card.password.notBlank")
-    @Size(min = 4, max = 4, message = "validation.card.password.size")
     @Pattern(regexp = "\\d{4}", message = "validation.card.password.pattern")
     @Schema(description = "Senha do cartão", example = "1234")
     @JsonProperty("senha")
