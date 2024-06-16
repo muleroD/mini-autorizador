92abff36379bae769f3b549544a6ed67dab91cee
Richard Mulero <mulerolp@gmail.com>
2024-06-16 02:12:30 -0300

    feat: criando consulta tratando cartão não encontrado

--- src/main/java/br/com/mulero/miniautorizador/domain/repository/CardRepository.java
+++ src/main/java/br/com/mulero/miniautorizador/domain/repository/CardRepository.java
@@ -1,9 +1,15 @@
 package br.com.mulero.miniautorizador.domain.repository;
 
 import br.com.mulero.miniautorizador.domain.entity.Card;
+import br.com.mulero.miniautorizador.infrastructure.exception.CardNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
+
+    default Card findOneByCardNumber(String cardNumber) {
+        return findOne(Card.builder().number(cardNumber).build().toExample())
+                .orElseThrow(CardNotFoundException::new);
+    }
 }
