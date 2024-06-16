3dcabd39a9ce8485f2f48640825a46e0b64f115e
Richard Mulero <mulerolp@gmail.com>
2024-06-16 03:22:40 -0300

    feat: criando metodo para exibir lista de erros ocorridos

--- src/main/java/br/com/mulero/miniautorizador/infrastructure/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/mulero/miniautorizador/infrastructure/handler/GlobalExceptionHandler.java
@@ -7,14 +7,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static br.com.mulero.miniautorizador.infrastructure.config.I18nConfig.RESOURCE_BUNDLE;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    public static final String DEFAULT_MESSAGE = "error.internal.server";
 
     @ExceptionHandler(CardAlreadyExistsException.class)
     public ResponseEntity<Object> handleCardAlreadyExistsException(CardAlreadyExistsException ex) {
@@ -23,29 +30,44 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TransactionException.class)
     public ResponseEntity<ProblemDetail> handleTransactionalException(TransactionException ex) {
-        return buildProblemDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage(), ex);
+        return buildProblemDetail(HttpStatus.UNPROCESSABLE_ENTITY, ex, ex.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ProblemDetail> handleConstraintViolationException(ConstraintViolationException ex) {
-        return buildProblemDetail(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        return buildProblemDetail(HttpStatus.BAD_REQUEST, ex, ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        String messages = ex.getBindingResult().getAllErrors().stream()
-                .map(ObjectError::getDefaultMessage)
-                .reduce((m1, m2) -> m1 + " | " + m2)
-                .orElse(ex.getMessage());
+        String message = RESOURCE_BUNDLE.getString("error.validation.list");
 
-        return buildProblemDetail(HttpStatus.BAD_REQUEST, messages, ex);
+        List<String> errors = ex.getBindingResult().getAllErrors().stream().map(objectError -> {
+            String errorMessage = Optional.ofNullable(objectError.getDefaultMessage()).orElse(DEFAULT_MESSAGE);
+            return RESOURCE_BUNDLE.getString(errorMessage);
+        }).toList();
+
+        return buildProblemDetail(HttpStatus.BAD_REQUEST, ex, message, errors);
     }
 
-    private ResponseEntity<ProblemDetail> buildProblemDetail(HttpStatus status, String message, Exception ex) {
+    private ResponseEntity<ProblemDetail> buildProblemDetail(HttpStatus status, Exception ex, String message, List<String> errors) {
         log.error(message, ex);
 
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, message);
+        ProblemDetail problemDetail = getProblemDetail(status, message);
+        problemDetail.setProperty("errors", errors);
         return new ResponseEntity<>(problemDetail, status);
     }
 
+    private ResponseEntity<ProblemDetail> buildProblemDetail(HttpStatus status, Exception ex, String message) {
+        log.error(message, ex);
+
+        ProblemDetail problemDetail = getProblemDetail(status, message);
+        return new ResponseEntity<>(problemDetail, status);
+    }
+
+    private ProblemDetail getProblemDetail(HttpStatus status, String message) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, message);
+        problemDetail.setProperty("timestamp", LocalDateTime.now().toString());
+        return problemDetail;
+    }
 }
