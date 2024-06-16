5891bc75f8df02a68b8666c5cc51882231d0b376
Richard Mulero <mulerolp@gmail.com>
2024-06-16 17:29:51 -0300

    feat: criando bean para PasswordEncoder e adicionando EnableMethodSecurity

--- src/main/java/br/com/mulero/miniautorizador/MiniAutorizadorApplication.java
+++ src/main/java/br/com/mulero/miniautorizador/MiniAutorizadorApplication.java
@@ -2,10 +2,8 @@ package br.com.mulero.miniautorizador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
-@EnableMethodSecurity(securedEnabled = true)
 public class MiniAutorizadorApplication {
 
     public static void main(String[] args) {
