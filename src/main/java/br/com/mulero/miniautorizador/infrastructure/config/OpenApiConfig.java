eee1b47d758874b0f70b95077c7cdb722af11734
Richard Mulero <mulerolp@gmail.com>
2024-06-16 17:32:36 -0300

    feat: atualizando swagger para incluir schema de basic auth

--- src/main/java/br/com/mulero/miniautorizador/infrastructure/config/OpenApiConfig.java
+++ src/main/java/br/com/mulero/miniautorizador/infrastructure/config/OpenApiConfig.java
@@ -1,17 +1,25 @@
 package br.com.mulero.miniautorizador.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    public static final String BASIC = "basic";
+    public static final String BASIC_SCHEME = "basicScheme";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .info(getInfo());
+                .info(getInfo())
+                .components(getSecuritySchemes())
+                .addSecurityItem(new SecurityRequirement().addList(BASIC_SCHEME));
     }
 
     private Info getInfo() {
@@ -19,5 +27,10 @@ public class OpenApiConfig {
                 .title("Mini Autorizador")
                 .description("Mini Autorizador")
                 .version("1.0.0");
+    }
+
+    private Components getSecuritySchemes() {
+        SecurityScheme scheme = new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme(BASIC);
+        return new Components().addSecuritySchemes(BASIC_SCHEME, scheme);
     }
 }
