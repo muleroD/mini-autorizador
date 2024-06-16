5a3b939747e693712d5dbddb961a1d594fe4d6a6
Richard Mulero <mulerolp@gmail.com>
2024-06-16 19:02:51 -0300

    feat: criando estrutura base para testes das controllers

---
+++ src/test/java/br/com/mulero/miniautorizador/controller/CardControllerTest.java
@@ -0,0 +1,16 @@
+package br.com.mulero.miniautorizador.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void getBalanceByCardNumber() {
+    }
+}
