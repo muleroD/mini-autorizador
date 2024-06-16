987173be7aa9c63e3eaa129887da527f0a872895
Richard Mulero <mulerolp@gmail.com>
2024-06-16 01:51:24 -0300

    chore: removendo consultas não utilizadas

--- src/main/java/br/com/mulero/miniautorizador/domain/repository/CardRepository.java
+++ src/main/java/br/com/mulero/miniautorizador/domain/repository/CardRepository.java
@@ -2,18 +2,8 @@ package br.com.mulero.miniautorizador.domain.repository;
 
 import br.com.mulero.miniautorizador.domain.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, Long> {
-
-    @Query("select true from Card c where c.number = :cardNumber")
-    Optional<Boolean> findFirstByNumber(String cardNumber);
-
-    @Query("select c.password from Card c where c.number = :cardNumber")
-    Optional<String> findPasswordByNumber(String cardNumber);
-
 }
