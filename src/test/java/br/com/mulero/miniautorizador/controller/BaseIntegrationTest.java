36bd612fb3d9009251a6161747f106dc13a929f7
Richard Mulero <mulerolp@gmail.com>
2024-06-16 22:45:54 -0300

    feat: criando client para centralizar requests

--- src/test/java/br/com/mulero/miniautorizador/controller/BaseIntegrationTest.java
+++ src/test/java/br/com/mulero/miniautorizador/controller/BaseIntegrationTest.java
@@ -1,34 +1,21 @@
 package br.com.mulero.miniautorizador.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import br.com.mulero.miniautorizador.rest.MockMvcClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Base64;
-
-public abstract class BaseIntegrationTest {
-
-    protected ObjectMapper objectMapper;
+public abstract class BaseIntegrationTest extends MockMvcClient {
 
     private AutoCloseable closeable;
 
     @BeforeEach
-    public void openService() {
+    public void init() {
         closeable = MockitoAnnotations.openMocks(this);
-        objectMapper = new ObjectMapper();
     }
 
     @AfterEach
     public void closeService() throws Exception {
         closeable.close();
-    }
-
-    public String getBasicAuth() {
-        return "Basic " + Base64.getEncoder().encodeToString("username:password".getBytes());
-    }
-
-    public String getInvalidBasicAuth() {
-        return "Basic " + Base64.getEncoder().encodeToString("invalid:invalid".getBytes());
     }
 }
--- src/test/java/br/com/mulero/miniautorizador/controller/CardIntegrationTest.java
+++ src/test/java/br/com/mulero/miniautorizador/controller/CardIntegrationTest.java
@@ -2,38 +2,28 @@ package br.com.mulero.miniautorizador.controller;
 
 import br.com.mulero.miniautorizador.dto.CardDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.math.BigDecimal;
 
 import static br.com.mulero.miniautorizador.util.CardUtil.createDefaultCardDto;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
 class CardIntegrationTest extends BaseIntegrationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private final BigDecimal balance = new BigDecimal("500.00");
 
     @Test
     void create() throws Exception {
         CardDTO cardBody = createDefaultCardDto();
 
-        MvcResult result = mockMvc.perform(post("/cartoes")
-                        .header(HttpHeaders.AUTHORIZATION, getInvalidBasicAuth())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(cardBody)))
-                .andReturn();
+        MvcResult result = performPost("/cartoes", cardBody);
 
         assertEquals(HttpStatus.CREATED.value(), result.getResponse().getStatus());
 
